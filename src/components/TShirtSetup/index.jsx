8a181b57e0e8558eeaa81975975c5b2a199b0cf1
Filip Jirsák <filip@jirsak.org>
2024-05-11 16:15:35 +0200

    Příklad možného řešení

--- src/components/TShirtSetup/index.jsx
+++ src/components/TShirtSetup/index.jsx
@@ -1,10 +1,10 @@
-import './style.css';
+import "./style.css";
 
-export const TShirtSetup = () => {
+export const TShirtSetup = ({ onTypeChange, onColorChange, onPrintChange }) => {
   return (
     <form className="tshirt-setup">
       <label className="tshirt-setup__label">Typ:</label>
-      <select className="tshirt-setup__input">
+      <select className="tshirt-setup__input" onChange={event => onTypeChange(event.target.value)}>
         <option value="normalShortSleeve">Krátký rukáv</option>
         <option value="tallShortSleeve">Krátký rukáv (prodloužené)</option>
         <option value="normalLongSleeve">Dlouhý rukáv</option>
@@ -14,15 +14,20 @@ export const TShirtSetup = () => {
       </select>
 
       <label className="tshirt-setup__label">Barva:</label>
-      <select className="tshirt-setup__input">
+      <select className="tshirt-setup__input" onChange={event => onColorChange(event.target.value)}>
         <option value="yellow">Žlutá</option>
         <option value="red">Červená</option>
         <option value="blue">Modrá</option>
         <option value="green">Zelená</option>
       </select>
-    
+
       <label className="tshirt-setup__label">Potisk:</label>
-      <input className="tshirt-setup__input" type="text" defaultValue="Ahoj světe" />
+      <input
+        className="tshirt-setup__input"
+        type="text"
+        defaultValue="Ahoj světe"
+        onChange={event => onPrintChange(event.target.value)}
+      />
     </form>
   );
 };
